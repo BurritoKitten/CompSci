1a759a3a26b447fe0cfeec77ca35ea87b6f6712d
BurritoKitten <burritokitten7@gmail.com>
2023-09-22 12:04:25 -0400

    assertion modifications

--- MathEx.java
+++ MathEx.java
@@ -2,19 +2,19 @@ public class MathEx
 {   
     public static void main(String[] args)
     {
-        assert (swap(123  )) == 132  : "swap failed on trial one";
-        assert (swap(1234 )) == 1243 : "swap failed on trial two";
+        assert (swap(123  )) == 132  : "swap failed on trial one"  ;
+        assert (swap(1234 )) == 1243 : "swap failed on trial two"  ;
         assert (swap(12345)) == 12354: "swap failed on trial three";
         
         assert (timeLeft(1, 15, 4, 36)).equals ("3 hours and 21 minutes."): "timeLeft failed on trial one";
         assert (timeLeft(1, 34, 8, 20)).equals ("6 hours and 46 minutes."): "timeLeft failed on trial two";
         
-        assert (dayOfWeek(0, 1 )) == 0: "dayOfWeek failed on trial one";
-        assert (dayOfWeek(0, 14)) == 6: "dayOfWeek failed on trial two";
+        assert (dayOfWeek(0, 1 )) == 0: "dayOfWeek failed on trial one"  ;
+        assert (dayOfWeek(0, 14)) == 6: "dayOfWeek failed on trial two"  ;
         assert (dayOfWeek(6, 22)) == 6: "dayOfWeek failed on trial three";
-        assert (dayOfWeek(5, 4 )) == 1: "dayOfWeek failed on trial four";
-        assert (dayOfWeek(1, 24)) == 3: "dayOfWeek failed on trial five";
-        assert (dayOfWeek(2, 1 )) == 2: "dayOfWeek failed on trial six";
+        assert (dayOfWeek(5, 4 )) == 1: "dayOfWeek failed on trial four" ;
+        assert (dayOfWeek(1, 24)) == 3: "dayOfWeek failed on trial five" ;
+        assert (dayOfWeek(2, 1 )) == 2: "dayOfWeek failed on trial six"  ;
 
         System.out.println("Success! Error below to prove valid syntax used for assert.");
         assert (false): "valid syntax proof. NOT AN ERROR!";
@@ -27,15 +27,16 @@ public class MathEx
     /**returns amount of time between two given times. (ex: 4:20-3:40="0 hours and 40 minutes")*/
     static String timeLeft (final int curHour, final int curMin, final int depHour, final int depMin) 
     {
-        assert ((curHour <= depHour & curMin <= depMin) || (curHour <= (depHour - 1) & curMin > depMin)): "invalid input supplied for timeLeft. deptime should be greater than curtime.";
-        final int outHour = (curMin <= depMin) ? (depHour - curHour) : (depHour - 1 - curHour);
-        final int outMin  = (curMin <= depMin) ? (depMin  - curMin ) : (depMin + 60 - curMin );
+        assert ((curHour <= depHour & curMin <= depMin) || (curHour < depHour & curMin > depMin)): "invalid input supplied for timeLeft. deptime should be greater than curtime.";
+        final int outHour = (curMin <= depMin) ? (depHour - curHour) : (depHour - 1  - curHour);
+        final int outMin  = (curMin <= depMin) ? (depMin  - curMin ) : (depMin  + 60 - curMin );
         return outHour + " hours and " + outMin + " minutes.";     
     }
     /**returns the current day of week given the day of week the month started on and current date. (0=Sunday, 1=Monday, etc.)*/
     static int dayOfWeek(final int firstDay, final int curDate)
     {
         assert (firstDay < 7): "please provide a valid first day of week. 0=Sunday, 1=Monday, etc.";
+        assert (firstDay >=0 & curDate >=0): "dayOfWeek cannot handle a negative date.";
         return (((curDate-1)+firstDay)%7);
     }
 }
