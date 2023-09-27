c4b5d76d0ae471f172617fb981466daf31260ee4
BurritoKitten <burritokitten7@gmail.com>
2023-09-27 09:43:05 -0400

    shorp

---
+++ Sharp.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HelloWorld
+{
+    class Sharp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
