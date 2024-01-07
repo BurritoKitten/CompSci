89caa89e4ce1d2ad94d845050819def19ef63444
BurritoKitten <burritokitten7@gmail.com>
2024-01-07 17:01:05 -0500

    Format testing

--- Fraction.cpp
+++ Fraction.cpp
@@ -1,4 +1,5 @@
 #include <cstdlib>
+#include <iostream>
 
 class Fraction{
     private:
@@ -11,13 +12,13 @@ class Fraction{
     {}
     Fraction(){}
 
-    void numerator(int numin){numerator_ = numin;}
+    void numerator(int numerator_){this->numerator_ = numerator_;}
     int numerator(){return numerator_;}
-    void denominator(int numin){denominator_ = numin;}
+    void denominator(int denominator_){this->denominator_ = denominator_;}
     int denominator(){return denominator_;}
     double decimalValue(){return (double)numerator_/denominator_;}
     
-    void decimalValue(double target, double precision, int denominator = 1)
+    void decimalValue(double target, double precision)
     {
         Fraction workingFraction(0, 1);
         if (precision>=1)
@@ -26,17 +27,23 @@ class Fraction{
                 precision = 1;
                 for (;i>0;i--){precision/=10;}
             }
+        workingFraction.decimalValueGivenBase(target, 1);
         while (std::abs(target - workingFraction.decimalValue()) > precision)
         {
-            workingFraction.denominator(workingFraction.denominator()+1);
-            workingFraction.numerator((int)(workingFraction.denominator()*target+0.5));
+            workingFraction.decimalValueGivenBase(target, workingFraction.denominator()+1);
         }
         *this = workingFraction;
     }
 
-    void decimalValueFromSize(double target, double precision, int denominator = 1)
+    void decimalValueGivenBase(double target, int base)
     {
-        Fraction workingFraction = Fraction();
+            this->denominator(base);
+            this->numerator((int)(base*target+0.5));
+    }
+
+    void decimalValueGivenMaxBase(double target, double precision, int denominator = 1)
+    {
+        Fraction workingFraction = Fraction(0, 0);
         Fraction currentBest = Fraction();
         while (workingFraction.denominator() <= precision)
         {
@@ -52,4 +59,13 @@ class Fraction{
 
         *this = currentBest;
     }
+
+    void printFraction()
+    {
+        std::cout << this->numerator(), " ";
+        std::cout << "/";
+        std::cout << this->denominator(), " ";
+    }
+
+    double deviation(double desiredValue){return desiredValue-(this->decimalValue());}
 };
--- FractionFromDecimalCalculator.cpp
+++ FractionFromDecimalCalculator.cpp
@@ -1,32 +1,29 @@
 #include <iostream>
 #include <string>
 #include "Fraction.cpp"
+#include <unistd.h>
 
-int main()
+int main(std::string args)
 {
-    while (true)
-    {
-        Fraction whatIsValue = Fraction();
-        // idea of project was to look for 1+(.5*sqrt(3))+(.5/sqrt(3)) aka the ratio of nested circles.
-        // approx. 2.15470053837925152901829756100391491129520350253058004370133658674167907642613285 or about 28/13
-        double testNum = 2.874512798465;
-        std::cout << "What number would you like to convert?\n";
-        std::cin >> testNum;
-        double testPrescision = 3;
-        std::cout << "What presicion would you like?\n";
-        std::cin >> testPrescision;
+    using namespace std;
+    Fraction whatIsValue = Fraction();
+    // idea of project was to look for 1+(.5*sqrt(3))+(.5/sqrt(3)) aka the ratio of nested circles.
+    // approx. 2.15470053837925152901829756100391491129520350253058004370133658674167907642613285 or about 28/13
+    double testNum = 2.874512798465;
+    cout << "What number would you like to convert?\n";
+    cin >> testNum;
+    double testPrescision = 3;
+    cout << "What presicion would you like?\n";
+    cin >> testPrescision;
+    if (args.find("denominator"))
         whatIsValue.decimalValue(testNum, testPrescision);
-        std::cout << "\nThe requested fraction is ";
-        std::cout << whatIsValue.numerator(), " ";
-        std::cout << "/";
-        std::cout << whatIsValue.denominator(), " ";
-        std::cout << "\nThis has a deviation of ";
-        std::cout << testNum-whatIsValue.decimalValue();
-        std::cout << " from the requested value.\n\nWould you like to quit? [Y/n]\n";
-        std::string quit;
-        std::cin >> quit;
-        if (!(quit=="n"||quit=="N"||quit=="No"||quit=="NO"||quit=="no"))
-        {return 0;}
-        std::cout << "\n\n";
-    }
+    cout << "\nThe requested fraction is ";
+    whatIsValue.printFraction();
+    cout << "\nThis has a value of ";
+    cout << whatIsValue.decimalValue();
+    cout << "\nThis has a deviation of about ";
+    cout << whatIsValue.deviation(testNum);
+    cout << " from the requested value.\n";
+    sleep(30);
+    return 0;
 }
